73b0f22b5d69b8762cc028f86dbda3abe3f1911a
mbiniosz <mbiniosz@users.noreply.github.com>
2019-02-10 13:14:14 +0100

    proba5

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -10,28 +10,27 @@ SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 def get_formatted(msg, imie, format):
     result = ""
     if format == PLAIN:
-        result = plain_text(msg, imie)
+        result = plain_text(mgs, imie)
     elif format == PLAIN_UP:
-        result = plain_text_upper_case(msg, imie)
+        result = plain_text_upper_case(mgs, imie)
     elif format == PLAIN_LO:
-        result = plain_text_lower_case(msg, imie)
+        result = plain_text_lower_case(mgs, imie)
     elif format == JSON:
-        result = format_to_json(msg, imie)
+        result = format_to_json(mgs, imie)
     return result
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie" : "' + imie + '" , "msg":' +
-            msg + '"}')
+    return ('{ "imie" : "' + imie + '" , "mgs":' +
+            mgs + '"}')
 
 
-def plain_text(msg, imie):
-    return imie + ' ' + msg
-
+def plain_text(mgs, imie):
+    return imie + ' ' + mgs
 
 def plain_text_upper_case(msg, imie):
-    return plain_text(msg.upper(), imie.upper())
+    return plain_text(mgs.upper(), imie.upper())
 
 
 def plain_text_lower_case(msg, imie):
-    return plain_text(msg.lower(), imie.lower())
+    return plain_text(mgs.lower(), imie.lower())
